d8ae5ee987ca72e70e1917e8766c30f026aa3aae
kaustubha-chaturvedi <kaustubhachaturvedi@outlook.com>
2026-01-20 20:32:19 +0530

    fix: update Windows static build process in release workflow to include libyuv and improve vcpkg caching; adjust LDFLAGS in avif.go, at this point i am trying random things

--- internal/formats/avif.go
+++ internal/formats/avif.go
@@ -7,7 +7,7 @@ package formats
 #cgo linux pkg-config: libavif
 #cgo darwin pkg-config: libavif
 #cgo windows CFLAGS: -I${SRCDIR}/../../embed/windows/include
-#cgo windows LDFLAGS: -L${SRCDIR}/../../embed/windows/lib -lavif -static
+#cgo windows LDFLAGS: -L${SRCDIR}/../../embed/windows/lib -lavif -lyuv -static
 #include <avif/avif.h>
 */
 import "C"
